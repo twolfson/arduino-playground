5dcaa0c90d25cdd8ed2d5c7c652fa6dc0afe00e8
Todd Wolfson <todd@twolfson.com>
2017-04-27 01:24:50 -0700

    Iterating on structs in C++

--- 06extra-rgb-led-smooth/src/main.cpp
+++ 06extra-rgb-led-smooth/src/main.cpp
@@ -1,25 +1,35 @@
 // Load in our dependencies
 #include "Arduino.h"
 
+// Define our structures
+struct color_t {
+  char red;
+  char green;
+  char blue;
+};
+
 // Define our constants
 // DEV: We use pins 9-11 as they're all PWM
 int RED_PIN = 9;
 int GREEN_PIN = 10;
 int BLUE_PIN = 11;
-int COLOR_ARRAY = [
-  {red: 255, green: 0, blue: 0},
-  {red: 0, green: 255, blue: 0},
-  {red: 0, green: 0, blue: 255},
-];
+color_t COLOR_RED;
+color_t COLOR_GREEN;
+color_t COLOR_BLUE;
+// color_t COLOR_ARRAY[3] = {
+//   color {red: 255, green: 0, blue: 0},
+//   {red: 0, green: 255, blue: 0},
+//   {red: 0, green: 0, blue: 255},
+// ];
 int STEP_DURATION = 10; // ms
 
 // Define our helpers
 // TODO: Is `char` the proper data-type? Does C++ have something like `byte` or `uint4`?
-void colorWrite(unsigned char red, unsigned char green, unsigned char blue) {
+void colorWrite(color_t color) {
   // Output our individual colors directly to their pins
-  analogWrite(RED_PIN, red);
-  analogWrite(GREEN_PIN, green);
-  analogWrite(BLUE_PIN, blue);
+  analogWrite(RED_PIN, color.red);
+  analogWrite(GREEN_PIN, color.green);
+  analogWrite(BLUE_PIN, color.blue);
 }
 
 // Define our main logic
@@ -37,27 +47,6 @@ void setup()
 void loop()
 {
   // Output basic colors
-  colorWrite(COLOR_ARRAY[0]['red'], 0, 0); // #FF0000 (red only)
-  delay(STEP_DURATION);
-  colorWrite(0, 255, 0); // #00FF00 (green only)
-  delay(STEP_DURATION);
-  colorWrite(0, 0, 255); // #0000FF (blue only)
-  delay(STEP_DURATION);
-
-  // Output complex colors
-  // Taken from: https://github.com/sunfounder/Sunfounder_SuperKit_for_Arduino/blob/d38b2f2d8d64a420f4123d0a9e2b713b705e19f4/06.RGB%20LED.ino#L27-L41
-  colorWrite(255, 0, 0); // Red
-  delay(STEP_DURATION);
-  colorWrite(237, 109, 0); // Orange
-  delay(STEP_DURATION);
-  colorWrite(255, 215, 0); // Yellow
-  delay(STEP_DURATION);
-  colorWrite(34, 139, 34); // Green
-  delay(STEP_DURATION);
-  colorWrite(0, 0, 255); // Blue
-  delay(STEP_DURATION);
-  colorWrite(0, 46, 90); // Indigo
-  delay(STEP_DURATION);
-  colorWrite(128, 0, 128); // Purple
+  colorWrite(COLOR_RED); // #FF0000 (red only)
   delay(STEP_DURATION);
 }
