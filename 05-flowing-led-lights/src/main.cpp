85e4c60e139bd30c5efcae9fc1f0186a66cbcac3
Todd Wolfson <todd@twolfson.com>
2017-04-27 00:55:23 -0700

    Completed full iteration and added representative comments

--- 05-flowing-led-lights/src/main.cpp
+++ 05-flowing-led-lights/src/main.cpp
@@ -4,7 +4,7 @@
 // Define our constants
 int START_LED_PIN = 11;
 int END_LED_PIN = 13;
-int STEP_DURATION = 250; // ms
+int STEP_DURATION = 100; // ms
 
 // Define our logic
 void setup()
@@ -19,7 +19,8 @@ void loop()
 {
   // For each of our LEDs in ascending order
   for (int led_pin = START_LED_PIN; led_pin <= END_LED_PIN; led_pin += 1) {
-    // Turn it on our LED
+    // Turn on our LED
+    // [ ][ ][ ] -> [x][ ][ ] ->... [x][x][x]
     digitalWrite(led_pin, HIGH);
 
     // Wait before turning on next LED
@@ -28,7 +29,28 @@ void loop()
 
   // For each of our LEDs in descending order
   for (int led_pin = END_LED_PIN; led_pin >= START_LED_PIN; led_pin -= 1) {
-    // Turn it off our LED
+    // Turn off our LED
+    // [x][x][x] -> [x][x][ ] ->... [ ][ ][ ]
+    digitalWrite(led_pin, LOW);
+
+    // Wait before turning off next LED
+    delay(STEP_DURATION);
+  }
+
+  // For each of our LEDs in descending order
+  for (int led_pin = END_LED_PIN; led_pin >= START_LED_PIN; led_pin -= 1) {
+    // Turn on our LED
+    // [ ][ ][ ] -> [ ][ ][x] ->... [x][x][x]
+    digitalWrite(led_pin, HIGH);
+
+    // Wait before turning on next LED
+    delay(STEP_DURATION);
+  }
+
+  // For each of our LEDs in ascending order
+  for (int led_pin = START_LED_PIN; led_pin <= END_LED_PIN; led_pin += 1) {
+    // Turn off our LED
+    // [x][x][x] -> [ ][x][x] ->... [ ][ ][ ]
     digitalWrite(led_pin, LOW);
 
     // Wait before turning off next LED
