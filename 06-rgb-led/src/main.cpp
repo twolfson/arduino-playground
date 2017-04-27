9e8e4782c16d8ff30a6d50d50d7fe7893b73fe2e
Todd Wolfson <todd@twolfson.com>
2017-04-27 01:11:13 -0700

    Added remaining complex LED colors

--- 06-rgb-led/src/main.cpp
+++ 06-rgb-led/src/main.cpp
@@ -6,6 +6,7 @@
 int RED_PIN = 9;
 int GREEN_PIN = 10;
 int BLUE_PIN = 11;
+int STEP_DURATION = 250; // ms
 
 // Define our helpers
 // TODO: Is `char` the proper data-type? Does C++ have something like `byte` or `uint4`?
@@ -29,9 +30,26 @@ void loop()
 {
   // Output basic colors
   colorWrite(255, 0, 0); // #FF0000 (red only)
-  delay(1000); // ms
+  delay(STEP_DURATION);
   colorWrite(0, 255, 0); // #00FF00 (green only)
-  delay(1000); // ms
+  delay(STEP_DURATION);
   colorWrite(0, 0, 255); // #0000FF (blue only)
-  delay(1000); // ms
+  delay(STEP_DURATION);
+
+  // Output complex colors
+  // Taken from: https://github.com/sunfounder/Sunfounder_SuperKit_for_Arduino/blob/d38b2f2d8d64a420f4123d0a9e2b713b705e19f4/06.RGB%20LED.ino#L27-L41
+  colorWrite(255, 0, 0); // Red
+  delay(STEP_DURATION);
+  colorWrite(237, 109, 0); // Orange
+  delay(STEP_DURATION);
+  colorWrite(255, 215, 0); // Yellow
+  delay(STEP_DURATION);
+  colorWrite(34, 139, 34); // Green
+  delay(STEP_DURATION);
+  colorWrite(0, 0, 255); // Blue
+  delay(STEP_DURATION);
+  colorWrite(0, 46, 90); // Indigo
+  delay(STEP_DURATION);
+  colorWrite(128, 0, 128); // Purple
+  delay(STEP_DURATION);
 }
