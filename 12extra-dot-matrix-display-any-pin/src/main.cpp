4b8805df5157f3d21d11993b1290bc02aa55802f
Todd Wolfson <todd@twolfson.com>
2017-08-05 04:27:09 -0700

    Added all of our cols

--- 12extra-dot-matrix-display-any-pin/src/main.cpp
+++ 12extra-dot-matrix-display-any-pin/src/main.cpp
@@ -16,34 +16,41 @@ const int CLOCK_PIN = 12;
 const int DATA_PIN = 11;
 const int LATCH_PIN = 8;
 
-register_mask_t ROW_1 = {register_1: 0b01000000, register_2: 0b00000000};
-register_mask_t ROW_2 = {register_1: 0b00000000, register_2: 0b00000000};
-register_mask_t ROW_3 = {register_1: 0b00000100, register_2: 0b00000000};
-register_mask_t ROW_4 = {register_1: 0b10000000, register_2: 0b00000000};
-register_mask_t ROW_5 = {register_1: 0b00100000, register_2: 0b00000000};
-register_mask_t ROW_6 = {register_1: 0b00000000, register_2: 0b00000000};
-register_mask_t ROW_7 = {register_1: 0b00000000, register_2: 0b00010000};
-register_mask_t ROW_8 = {register_1: 0b00000000, register_2: 0b00000100};
+register_mask_t  ROW_1 = {register_1: 0b01000000, register_2: 0b00000000};
+register_mask_t  ROW_2 = {register_1: 0b00010000, register_2: 0b00000000}; // DEAD
+register_mask_t  ROW_3 = {register_1: 0b00000100, register_2: 0b00000000};
+register_mask_t  ROW_4 = {register_1: 0b10000000, register_2: 0b00000000};
+register_mask_t  ROW_5 = {register_1: 0b00100000, register_2: 0b00000000};
+register_mask_t  ROW_6 = {register_1: 0b00001000, register_2: 0b00000000}; // DEAD
+register_mask_t  ROW_7 = {register_1: 0b00000000, register_2: 0b00010000};
+register_mask_t  ROW_8 = {register_1: 0b00000000, register_2: 0b00000100};
 
-register_mask_t COL_1 = {register_1: 0b00000000, register_2: 0b00000000};
-register_mask_t COL_2 = {register_1: 0b00000000, register_2: 0b00000000};
-register_mask_t COL_3 = {register_1: 0b00000000, register_2: 0b00000000};
-register_mask_t COL_4 = {register_1: 0b00000000, register_2: 0b00000000};
-register_mask_t COL_5 = {register_1: 0b00000000, register_2: 0b10000000};
-register_mask_t COL_6 = {register_1: 0b00000000, register_2: 0b00100000};
-register_mask_t COL_7 = {register_1: 0b00000000, register_2: 0b01000000};
-register_mask_t COL_8 = {register_1: 0b00000000, register_2: 0b00100000};
+// DEV: These black out an LED row, we need to invert them with the other columns to get their true value
+register_mask_t _COL_1 = {register_1: 0b00000000, register_2: 0b00000001};
+register_mask_t _COL_2 = {register_1: 0b00000001, register_2: 0b00000000};
+register_mask_t _COL_3 = {register_1: 0b00000000, register_2: 0b00000010};
+register_mask_t _COL_4 = {register_1: 0b00000000, register_2: 0b00001000};
+register_mask_t _COL_5 = {register_1: 0b00000010, register_2: 0b00000000};
+register_mask_t _COL_6 = {register_1: 0b00000000, register_2: 0b00100000};
+register_mask_t _COL_7 = {register_1: 0b00000000, register_2: 0b01000000};
+register_mask_t _COL_8 = {register_1: 0b00000000, register_2: 0b10000000};
 
-// 1: 0b00010000
-// 1: 0b00001000
-// 1: 0b00000010
-// 1: 0b00000001
-// 2: 0b10000000
-// 2: 0b01000000
-// 2: 0b00100000
-// 2: 0b00001000
-// 2: 0b00000010
-// 2: 0b00000001
+register_mask_t ALL_COLS = {
+  register_1: _COL_1.register_1 & _COL_2.register_1 & _COL_3.register_1 & _COL_4.register_1 & _COL_5.register_1 & _COL_6.register_1 & _COL_7.register_1 & _COL_8.register_1,
+  register_2: _COL_1.register_2 & _COL_2.register_2 & _COL_3.register_2 & _COL_4.register_2 & _COL_5.register_2 & _COL_6.register_2 & _COL_7.register_2 & _COL_8.register_2
+};
+register_mask_t COL_1 = {register_1: ALL_COLS.register_1 ^ _COL_1.register_1, register_2: ALL_COLS.register_1 ^ _COL_1.register_2};
+register_mask_t COL_2 = {register_1: ALL_COLS.register_1 ^ _COL_2.register_1, register_2: ALL_COLS.register_1 ^ _COL_2.register_2};
+register_mask_t COL_3 = {register_1: ALL_COLS.register_1 ^ _COL_3.register_1, register_2: ALL_COLS.register_1 ^ _COL_3.register_2};
+register_mask_t COL_4 = {register_1: ALL_COLS.register_1 ^ _COL_4.register_1, register_2: ALL_COLS.register_1 ^ _COL_4.register_2};
+register_mask_t COL_5 = {register_1: ALL_COLS.register_1 ^ _COL_5.register_1, register_2: ALL_COLS.register_1 ^ _COL_5.register_2};
+register_mask_t COL_6 = {register_1: ALL_COLS.register_1 ^ _COL_6.register_1, register_2: ALL_COLS.register_1 ^ _COL_6.register_2};
+register_mask_t COL_7 = {register_1: ALL_COLS.register_1 ^ _COL_7.register_1, register_2: ALL_COLS.register_1 ^ _COL_7.register_2};
+register_mask_t COL_8 = {register_1: ALL_COLS.register_1 ^ _COL_8.register_1, register_2: ALL_COLS.register_1 ^ _COL_8.register_2};
+
+
+// 1: 0b00010000 - DEAD
+// 1: 0b00001000 - DEAD
 
 const int ROWS = 8;
 const int IMAGES = 18;
@@ -80,8 +87,8 @@ void setup()
 void loop()
 {
   delay(1);
-  shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, ROW_2.register_1 | COL_6.register_1);
-  shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, ROW_2.register_2 | COL_6.register_2);
+  shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, ROW_1.register_1 | COL_1.register_1);
+  shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, ROW_1.register_2 | COL_1.register_2);
   digitalWrite(LATCH_PIN, HIGH);
   digitalWrite(LATCH_PIN, LOW);
 }
